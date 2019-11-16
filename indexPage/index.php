73fedb3383f15f377d71e2ff4116817444c69c6c
yrwid <mudri656@gmail.com>
2019-11-16 22:55:21 +0100

    clock

--- indexPage/index.php
+++ indexPage/index.php
@@ -91,9 +91,39 @@ EOT;
 				With us you are able to achieve each of these things and even more! </p>
 			</div>
 			
-			<div class="clock">
-			<p>   12:00:00  </p>
-			</div>
+			<div class="clock" id="MyClockDisplay" onload="showTime()"></div>
+			
+			<script>
+			function showTime(){
+			var date = new Date();
+			var h = date.getHours(); // 0 - 23
+			var m = date.getMinutes(); // 0 - 59
+			var s = date.getSeconds(); // 0 - 59
+			var session = "AM";
+    
+			if(h == 0){
+				h = 12;
+			}
+    
+			if(h > 12){
+				h = h - 12;
+				session = "PM";
+			}
+    
+			h = (h < 10) ? "0" + h : h;
+			m = (m < 10) ? "0" + m : m;
+			s = (s < 10) ? "0" + s : s;
+    
+			var time = h + ":" + m + ":" + s + " " + session;
+			document.getElementById("MyClockDisplay").innerText = time;
+			document.getElementById("MyClockDisplay").textContent = time;
+    
+			setTimeout(showTime, 1000);
+    
+		}
+
+		showTime();
+		</script>
 			
 			
 			<div class="socials"><a style="text-decoration:none; color:#096506;"  href='https://329elearning.aei.polsl.pl/tiwordpress2019/s121/'  >IT Blog</a></div> 
